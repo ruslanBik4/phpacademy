a93d2747fc2112edf5cfd10ed821c3f7a247493c
ruslan-bik <ruslan-bik@yandex.ru>
2016-06-16 21:02:11 +0300

    3.06.2016

--- musoboz/news.php
+++ musoboz/news.php
@@ -15,47 +15,98 @@
 
  $arrNews = array(
      [
-         '21/04/2016',
-         'Скоро концерты Машины Времени в Киеве',
-         'Легендарная группа Машина Времени скоро посетить город Киев 
+         'date' => '21/04/2016',
+         'title'=> 'Скоро концерты Машины Времени в Киеве',
+         'memo' => 'Легендарная группа Машина Времени скоро посетить город Киев 
                 и окрестности с юбилейными концертами и 
                 большими пьянками 
                 для всех любителей музыки',
+         'cost' => 120,
+         'permission' => '18+',
+         'isActual' => false
      ],
 
      [
-         '21/05/2016',
-         'Скоро концерты DDT ени в Киеве',
-         'Легендарная группа DDT скоро посетить город Киев 
+         'date' => '21/05/2016',
+         'title'=> 'Скоро концерты DDT ени в Киеве',
+         'memo' => 'Легендарная группа DDT скоро посетить город Киев 
                 и окрестности с юбилейными концертами и 
                 молебнами
                 для всех любителей музыки',
+         'cost' => 100,
+         'permission' => '41+',
+         'isActual' => false
      ],
      [
-         '18.06.2016',
-         'Скоро концерты Руки вверх! в Киеве',
-         'Приезд группы руки ВВерх! 
+         'date' => '18.06.2016',
+         'title'=> 'Скоро концерты Руки вверх! в Киеве',
+         'memo' => 'Приезд группы руки ВВерх! 
      отменяется',
+         'cost' => 0,
+         'permission' => '12+',
+         'isActual' => true
 
      ],
      [
-         '18.06.2016',
-        'Концерт группы "Океан Эльзы"',
-        ':Ltv-c!',
+         'date' =>  '18.06.2016',
+         'title'=> 'Концерт группы "Океан Эльзы"',
+         'memo' => ':Ltv-c!',
+         'cost' => 200,
+         'permission' => '0+',
+         'isActual' => true
      ],
 
  );
+
+ $summa = 0;
 ?>
-    <table>
+<table>
 <?php
- for ($i=0; $i < count($arrNews); $i++)
+foreach ($arrNews as $i => $arrSingleNews)
  {
-?>
+     if ($i == 0) {
+       echo '<tr">';
+
+     $key = array_keys( $arrSingleNews );
+     $keyI = 0;
+
+         do
+        {
+            echo "<td style='border-bottom:1px solid red;'><b>$key[$keyI]</b></td>";
+
+            $keyI++;
+
+        }
+         while ( $key[$keyI] != 'isActual');
+
+         echo '</tr>';
+     }
+
+     $summa += $arrSingleNews['cost'];
+ ?>
      <tr>
          <?php
-             for($j=0; $j < count($arrNews[$i]); $j++ )
+             foreach($arrSingleNews as $key => $value)
              {
-               echo "<td>{$arrNews[$i][$j]}</td>";
+               switch ($key) {
+                   case 'date':
+                       echo "<td><i>$value</i></td>";
+                       break;
+                   case 'title':
+                       echo "<td><b>$value</b></td>";
+                       break;
+                   case 'memo':
+                       echo "<td>$value</td>";
+                       break;
+                   case 'cost':
+                       echo "<td>$value</td>";
+                       break;
+                   case 'permission':
+                       echo "<td>$value</td>";
+                       break;
+                   default:
+
+               }
              }
          ?>
      </tr>
@@ -72,7 +123,7 @@
 
 ?>
      <tr>
-         <td> <?=$strPayable?> сайт</td>
+         <td> <?=$summa?> сайт</td>
          <td><?php echo STR_FOOTER; ?></td>
          <td> <?=isset($current_year) ? $current_year : 'неизвестный год'?></td>
      </tr>
